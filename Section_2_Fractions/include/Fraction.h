c311665fe802bcb8e82af331807d5a9771c5ebef
leylaiskandarli <zcapisk@ucl.ac.uk>
2025-01-24 16:23:44 +0000

    Added necessary functions to Section 2 of week 2 excercises

--- Section_2_Fractions/include/Fraction.h
+++ Section_2_Fractions/include/Fraction.h
@@ -1,2 +1,22 @@
+#ifndef FRACTION_H
+#define FRACTION_H
 #pragma once
 #include <string>
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+
+    void simplify();
+
+public: 
+
+    Fraction(int a, int b);
+    Fraction reciprocal() const;
+    Fraction multiply(int a) const;
+    double toDouble() const; 
+    std::string toString() const;
+};
+
+#endif
--- Section_2_Fractions/src/Fraction.cpp
+++ Section_2_Fractions/src/Fraction.cpp
@@ -1,1 +1,47 @@
-#include "Fraction.h"
+#include "../include/Fraction.h"
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <numeric>
+
+
+Fraction::Fraction(int a, int b) : numerator(a), denominator(b) {
+    if (b == 0) {
+        throw std::invalid_argument("Denominator can not be zero.");
+    }
+    simplify();
+}
+
+void Fraction::simplify() {
+    int gcd = std::gcd(numerator, denominator);
+    numerator /= gcd;
+    denominator /= gcd; 
+
+    if (denominator < 0) {
+        numerator = -numerator;
+        denominator = -denominator;
+    }
+}
+
+Fraction Fraction::reciprocal() const{
+    if (numerator == 0) {
+        throw std::domain_error("Can not take reciprocal of zero.");
+    }
+    return Fraction(denominator, numerator);
+}
+
+Fraction Fraction::multiply(int a) const{
+    return Fraction(numerator*a, denominator);
+
+} 
+
+double Fraction::toDouble() const{
+    return static_cast<double>(numerator) / denominator;
+}
+
+std::string Fraction::toString() const{
+    if (numerator == 0) {
+        return "0";
+    }
+    return std::to_string(numerator) + "/" + std::to_string(denominator);
+}
